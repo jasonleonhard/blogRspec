9311cc1c4d5e8b83a6f908f752250b261fb247e8
un5t0ppab13 <devbrights@gmail.com>
2015-08-05 14:04:30 -0700

    pre-merge: temp into master ;

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :model do
   # pending "add some examples to (or delete) #{__FILE__}"
-  before(:all)do 
-    @post = Post.new(body:"My Body", title:"My Title")
+  before(:all)do # before action allows instance variable to be available throughout the test process
+    @post = Post.new(body:"My Body", title:"My Title") # instance variable
   end
   it "should have a matching body" do
-    expect(@post.body).to eq("Your Body") # will fail #rspec # 1 example, 1 failure
-    # expect(@post.body).to eq("My Body") # will pass #rspec # 1 example, 0 failures
+    # expect(@post.body).to eq("Your Body") # will fail #rspec # 1 example, 1 failure
+    expect(@post.body).to eq("My Body") # will pass #rspec # 1 example, 0 failures
   end
 end
